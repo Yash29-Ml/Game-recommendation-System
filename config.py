7e55c9f1ddfc15abe84f50651b7ca49cc90d0761
Yash29-ML <shindehimanshu446@gmail.com>
2025-11-01 21:35:11 +0530

    Initial commit: Game Master Flask Application

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-STEAM_API_KEY = "E579F61D0F6B642C45C82A7A946D5EF7"
+import os
+
+STEAM_API_KEY = os.environ.get("STEAM_API_KEY", "E579F61D0F6B642C45C82A7A946D5EF7")
 
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "python_user",
-    "password": "aizen",
-    "database": "game_recommender"
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "python_user"),
+    "password": os.environ.get("DB_PASSWORD", "aizen"),
+    "database": os.environ.get("DB_NAME", "game_recommender")
 }
